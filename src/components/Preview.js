24641ebe0ff34cb1d6500ff8e7bc4a2628e2e566
Brian Peiris <brianpeiris@gmail.com>
2022-08-02 00:08:09 -0400

    update build

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -69,7 +69,7 @@ const Preview = forwardRef(function Preview({ texture }, ref) {
     canvasTexture.anisotropy = maxAnisotropy;
     map.current = canvasTexture;
 
-    new GLTFLoader().load("nametag-2.glb", (gltf) => {
+    new GLTFLoader().load("nametag.glb?v=1", (gltf) => {
       gltf.scene.traverse((object) => {
         if (object.material?.map && !object.name.includes("webcam_frame")) {
           object.material.map = canvasTexture;
